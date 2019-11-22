0afb8e78d0f6ba4d56a299c75f8ddc740c037648
Renay Earnshaw <renayearnshaw@gmail.com>
2019-11-22 15:59:50 +0000

    Added endpoints for reading of task resources (last commit was actually users)

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+// Users
 app.get('/users', (req, res) => {
     User.find({}).then((users) => {
         res.send(users)
@@ -36,6 +37,28 @@ app.post('/users', (req, res) => {
         res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+// Tasks
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send()
     })
 })
 
