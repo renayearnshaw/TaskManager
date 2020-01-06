021358bc8badef2fe9a0a4912932373e12eab4af
Renay Earnshaw <renayearnshaw@gmail.com>
2020-01-06 11:32:36 +0000

    Added updating of a user using HTTP patch method

--- src/index.js
+++ src/index.js
@@ -18,6 +18,16 @@ app.get('/users', async (req, res) => {
     }
 })
 
+app.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send()
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
     try {
@@ -31,11 +41,21 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
+app.patch('/users/:id', async (req, res) => {
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const receivedUpdates = Object.keys(req.body)
+    const isValidOperation = receivedUpdates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
     try {
-        await user.save()
-        res.status(201).send()
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
     } catch (error) {
         res.status(400).send(error)
     }
