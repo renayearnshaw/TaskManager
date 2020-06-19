f8211c8b91de3835d0f3178cd9a9187ddf12188c
Renay Earnshaw <renayearnshaw@gmail.com>
2020-06-19 16:57:14 +0100

    Clear the database before each test, and add a user

--- test/user.test.js
+++ test/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Barney',
+    email: 'barney@example.com',
+    password: 'woof8888'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('Should sign up a new user', async () => {
         email: 'fred@example.com',
         password: 'MyPass777!'
     }).expect(201)
+})
+
+test('Should log in an existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Shouldn\'t log an invalid user in', async () => {
+    await request(app).post('/users/login').send({
+        email: 'renay@example.com',
+        password: 'anyOldPassword'
+    }).expect(400)
 })
